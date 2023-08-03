1c36825543048816cb1c5362d8008f97d3e6d0e7
ali sadeghy <alixvar68@gmail.com>
2023-08-03 22:08:28 +0330

    add some responsive style to mainPage black frame

--- app/page.tsx
+++ app/page.tsx
@@ -9,25 +9,28 @@ export default function Home() {
   return (
     <main className="px-4">
       {/* Background Frame  */}
-      <div className="bg-black w-full px-4 py-6  rounded-xl flex flex-col items-center justify-center md:flex-row">
+      <div className="bg-black w-full h-fit p-6  rounded-xl flex flex-col items-center justify-center md:flex-row">
         {/* Top Section  */}
-        <div className="w-full h-full max-w-md   bg-slate-200 rounded-xl p-4  flex flex-col items-center gap-8 md:flex-1">
+        <div className="h-full w-full md:w-3/5 bg-slate-200 rounded-xl p-4 lg:p-8 lg:py-16 flex flex-col items-center lg:items-start gap-8 md:flex-1">
           {/* Title  */}
           <div className="flex flex-col w-full ">
-            <p className="text-3xl">Unlock Your Ideal</p>
-            <div className="flex gap-2 text-4xl w-full items-center justify-center font-bold">
-              <p>Living</p>
-              <p className="text-blue-700">Space!</p>
+            <p className="text-3xl lg:text-6xl font-light">Unlock Your Ideal</p>
+            <div className="flex gap-2 text-4xl w-full items-center mt-3 justify-center font-bold">
+              <p className="lg:text-5xl">Living</p>
+              <p className="text-blue-700 lg:text-5xl">Space!</p>
             </div>
           </div>
           {/* Description  */}
-          <p className="text-base text-slate-400">
+          <p className="text-base text-slate-400 max-w-xs">
             Whether you're looking to buy,sell,or rent,we are here to assist you
             every step of the wey.
           </p>
-          <div className="w-full flex justify-between ">
+          <div className="w-full flex gap-4 ">
             <button className="bg-blue-700 text-white px-6 py-[7px] rounded-full">
               Contact
+            </button>
+            <button className="hidden lg:inline border border-blue-700 text-blue-700 font-semibold px-6 py-[7px] rounded-full">
+              Register
             </button>
           </div>
         </div>
@@ -38,13 +41,13 @@ export default function Home() {
           <div className="h-full bg-black w-[30%] rounded-l-full" />
         </div>
         {/* Bottom Section  */}
-        <div className="w-full h-full max-w-md  flex items-center justify-center rounded-xl p-4 md:flex-1 bg-red-500">
+        <div className="w-full h-full relative flex items-center justify-center rounded-xl md:w-2/5">
           <Image
             src={building}
             alt=""
             width={1000}
             height={1000}
-            className="w-full h-full object-fill"
+            className="object-cover w-full h-full md:hidden"
           />
         </div>
       </div>
