20c799c5cec2a3d1949a999bbce96f2377accf32
ali sadeghy <alixvar68@gmail.com>
2023-08-08 20:42:28 +0330

    add responsive style to about and search

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -1,8 +1,8 @@
 function AboutUs() {
   return (
-    <div className="mt-16 px-16 w-full flex items-start justify-between">
-      <p className="font-semibold text-4xl">About Us</p>
-      <p className="text-slate-500 max-w-3xl text-xl">
+    <div className="mt-16 px-4 lg:px-16 w-full flex flex-col lg:flex-row items-start justify-between gap-4 lg:gap-0">
+      <p className="font-semibold text-3xl lg:text-4xl">About Us</p>
+      <p className="text-slate-500 max-w-3xl text-lg font-semibold lg:text-xl">
         At <span className="text-blue-700 font-semibold">LIVING</span>,we are
         your trusted real state experts.Our mission is simple:to help you find
         your dream home or make a successful property investment. with years of
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -8,10 +8,10 @@ import LongBuild from "@/assets/longbuild.svg";
 function SearchBox() {
   return (
     <section className="relative flex flex-col items-center justify-center my-16">
-      <div className="bg-black w-full pt-40 px-4 pb-48  text-white rounded-xl flex items-center justify-center ">
-        <div className="absolute left-36 -top-32 flex gap-10 items-center">
+      <div className="bg-black w-full lg:pt-40 px-4 pb-28 pt-20 lg:pb-48 text-white rounded-xl flex items-center justify-center">
+        <div className="absolute lg:left-36 -top-12 lg:-top-32 flex lg:gap-10 items-center">
           {/* Bulding Image  */}
-          <div className="relative max-w-sm">
+          <div className="relative ml-2 lg:ml-0 max-w-[170px] lg:max-w-sm">
             <Image
               src={LongBuild}
               width={350}
@@ -21,48 +21,48 @@ function SearchBox() {
             />
           </div>
           {/* User and Units Info  */}
-          <div className="bg-customSlate/30 rounded-3xl flex items-center px-20 py-8 h-fit gap-10 ml-10 mt-20">
+          <div className="bg-customSlate/30 rounded-3xl flex items-center lg:px-20 lg:py-8 h-fit gap-2 lg:gap-10 lg:ml-10 mt-10 lg:mt-20 p-4 min-w-[240px]">
             <div className="text-center">
-              <p className="text-white font-semibold text-4xl">
+              <p className="text-white font-semibold text-sm lg:text-4xl">
                 8K<span className="text-sky-600">+</span>
               </p>
-              <p className="text-gray-300">Units Sold</p>
+              <p className="text-gray-300 text-xs">Units Sold</p>
             </div>
             <div className="text-center">
-              <p className="text-white font-semibold  text-4xl">
+              <p className="text-white font-semibold text-sm  lg:text-4xl">
                 12K<span className="text-sky-600">+</span>
               </p>
-              <p className="text-gray-300">Units Ready</p>
+              <p className="text-gray-300 text-xs">Units Ready</p>
             </div>
             <div className="text-center">
-              <p className="text-white font-semibold  text-4xl">
+              <p className="text-white font-semibold text-sm lg:text-4xl">
                 9K<span className="text-sky-600">+</span>
               </p>
-              <p className="text-gray-300">Customers</p>
+              <p className="text-gray-300 text-xs">Customers</p>
             </div>
           </div>
         </div>
       </div>
       {/* Search Bar  */}
-      <div className="bg-white px-16  shadow-lg py-10 rounded-xl flex flex-col gap-4 w-[80%]  absolute -bottom-32 ">
+      <div className="bg-white p-4 lg:px-16  shadow-lg lg:py-10 rounded-xl flex flex-col gap-4 lg:w-[80%] w-[90%]  absolute -bottom-20 lg:-bottom-32 ">
         {/* Title  */}
         <div className="flex gap-2 items-center">
           <FiSearch size={22} className="text-slate-500" />
-          <p className="text-slate-500 text-2xl">
+          <p className="text-slate-500 text-md lg:text-2xl">
             Let's find your ideal house...
           </p>
         </div>
         {/* Search input  */}
         <div className="flex items-center gap-4 justify-between ">
-          <div className="flex items-center  px-3 bg-customSlate rounded-lg">
+          <div className="flex items-center  px-3 bg-customSlate rounded-lg w-full lg:w-fit">
             <IoLocationOutline size={25} color={"rgb(29 78 216)"} />
             <input
               type="text"
-              className="bg-customSlate p-2 py-3 w-full focus:outline-none placeholder:text-lg"
+              className="bg-customSlate p-2 lg:py-3 w-full focus:outline-none lg:placeholder:text-lg flex-1"
               placeholder="Loacation"
             />
           </div>
-          <div className="flex items-center  px-3 bg-customSlate rounded-lg">
+          <div className="hidden lg:flex items-center  px-3 bg-customSlate rounded-lg">
             <FiHome size={25} color={"rgb(29 78 216)"} />
             <input
               type="text"
@@ -70,7 +70,7 @@ function SearchBox() {
               placeholder="Loacation"
             />
           </div>
-          <div className="flex items-center  px-3 bg-customSlate rounded-lg">
+          <div className="hidden lg:flex items-center  px-3 bg-customSlate rounded-lg">
             <BsCash size={25} color={"rgb(29 78 216)"} />
             <input
               type="text"
